7a8c3f61920d43467b22600efd2fb2e4c5006c98
ronaldo.serra <ronaldo.serra@cencosud.cl>
2024-04-22 22:14:44 -0400

    functions calculator

--- js/main.js
+++ js/main.js
@@ -1,8 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll("button");
+    const inputResult = document.getElementById("screen");
+    let number1 = '';
+    let number2 = '';
+    let operator = '';
 
     buttons.forEach(button => {
-        button.addEventListener("click", handleClick);
+        button.addEventListener("click", function () {
+            const buttonText = button.textContent;
+            inputResult.value += buttonText;
+
+            if (isInteger(buttonText)) {
+                if (operator === '') {
+                    number1 += buttonText;
+                } else {
+                    number2 += buttonText;
+                }
+
+            } else if (isOperator(buttonText)) {
+                operator = buttonText;
+
+            } else if (buttonText == '=') {
+                let results = '';
+                switch (operator) {
+                    case '+':
+                        results = number1 + number2;
+                        break;
+                    case '-':
+                        results = number1 - number2;
+                        break;
+                    case '*':
+                        results = number1 * number2;
+                        break;
+                    case '/':
+                        results = number1 / number2;
+                        break;
+                    default:
+                        break;
+                }
+
+                inputResult.value = results.toString();
+
+                number1 = '';
+                number2 = '';
+                operator = '';
+            }
+        });
     });
 });
 
@@ -11,30 +54,23 @@ function appendNumber(number) {
     const inputResult = document.getElementById("screen");
     inputResult.value += number;
 }
-// Funcion para obtener el boton que se esta clickeando y mostrarlo en pantalla input
+
+// Funcion para obtener el valor del boton que se esta clickeando y mostrarlo en pantalla input
 function handleClick(event) {
     const buttonText = event.target.textContent;
-    
-    appendNumber(buttonText)
-}
-
-function sum(number1, number2) {
-    return number1 + number2
-}
-
-function substract(number1, number2) {
-    return number1 - number2
-}
-
-function multiply(number1, number2) {
-    return number1 * number2
-}
-
-function divide(number1, number2) {
-    return number1 / number2
+    appendNumber(buttonText);
 }
 
 function clearScreen() {
     const inputResult = document.getElementById("screen");
-    inputResult.value = ''
+    inputResult.value = '';
 }
+
+function isOperator(operator) {
+    return ['+', '-', '*', '/'].includes(operator);
+}
+
+function isInteger(text) {
+    return /^\d+$/.test(text);
+}
+
